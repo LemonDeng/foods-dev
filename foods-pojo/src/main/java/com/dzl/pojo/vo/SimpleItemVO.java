6acf36ca13a6c38f4261dfe736b608fd795096c1
DZL <593529172@qq.com>
2021-03-16 09:56:10 +0800

    查询每个一级分类下的最新6条商品数据 收尾

---
+++ foods-pojo/src/main/java/com/dzl/pojo/vo/SimpleItemVO.java
@@ -0,0 +1,38 @@
+package com.dzl.pojo.vo;
+
+import java.util.List;
+
+/**
+ * 最新商品VO
+ *
+ */
+public class SimpleItemVO {
+
+    private Integer itmeId;
+    private String itemName;
+    private String itemUrl;
+
+    public Integer getItmeId() {
+        return itmeId;
+    }
+
+    public void setItmeId(Integer itmeId) {
+        this.itmeId = itmeId;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public String getItemUrl() {
+        return itemUrl;
+    }
+
+    public void setItemUrl(String itemUrl) {
+        this.itemUrl = itemUrl;
+    }
+}
