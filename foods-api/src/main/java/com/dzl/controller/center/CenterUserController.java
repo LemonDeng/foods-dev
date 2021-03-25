efcebcaf001c5133c3474c927856457597bfb339
DZL <593529172@qq.com>
2021-03-25 15:21:39 +0800

    更新用户头像到数据库

--- foods-api/src/main/java/com/dzl/controller/center/CenterUserController.java
+++ foods-api/src/main/java/com/dzl/controller/center/CenterUserController.java
@@ -135,10 +135,13 @@ public class CenterUserController extends BaseController {
                     Date date = new Date();
                     String format = simpleDateFormat.format(date);
                     String newFileName = "face-" + userId +"-"+ format +"." + suffix;
-           
+
 
                     // 上传的头像最终保存的位置
                     String finalFacePath = fileSpace + uploadPathPrefix + File.separator + newFileName;
+
+                    // 用于提供给web服务访问的地址
+                    uploadPathPrefix += ("/" + newFileName);
 
                     File outFile = new File(finalFacePath);
                     //判断传进来的路径是否存在
@@ -169,8 +172,26 @@ public class CenterUserController extends BaseController {
         } else {
             return DZLJSONResult.errorMsg("文件不能为空！");
         }
-            return DZLJSONResult.ok();
+
+        // 获取图片服务地址
+        String imageServerUrl = fileUpload.getImageServerUrl();
+
+        // 由于浏览器可能存在缓存的情况，所以在这里，我们需要加上时间戳来保证更新后的图片可以及时刷新
+        String finalUserFaceUrl = imageServerUrl + uploadPathPrefix
+                + "?t=" + DateUtil.getCurrentDateString(DateUtil.DATE_PATTERN);
+
+        // 更新用户头像到数据库
+        Users userResult = centerUserService.updateUserFace(userId, finalUserFaceUrl);
+
+        userResult = setNullProperty(userResult);
+        CookieUtils.setCookie(request, response, "user",
+                JsonUtils.objectToJson(userResult), true);
+
+        // TODO 后续要改，增加令牌token，会整合进redis，分布式会话
+
+        return DZLJSONResult.ok();
     }
+
 
 
 }
